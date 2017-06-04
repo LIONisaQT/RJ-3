002f0400031b5e3ce616c08d75ceaf56ad19e159
LIONisaQT <ryan.shee@gmail.com>
2017-06-04 14:56:57 -0700

    didn't save lol

--- TEST/js/states/load.js
+++ TEST/js/states/load.js
@@ -5,7 +5,6 @@ var loadState = {
 		game.load.images(['pipe', 'steam', 'player', 'heart', 'enemy', 'wall', 'ooze', 'leak', 'instructions'],
 		['pipeProto.png', 'steamProto.png', 'playerProto.png', 'heartProto.png', 'enemyProto.png', 'wallProto.png', 'oozeProto.png', 'leakProto.png', 'instructions.png']);
 		game.load.tilemap('level', 'level1.json', null, Phaser.Tilemap.TILED_JSON);
-		// game.load.spritesheet('tilesheet', 'prototypeStage.png', 40, 40);
 		game.load.image('tiles', 'brickpipe.png');
 		game.load.image('playbutton', 'playButton.png');
 
